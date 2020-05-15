cc12e068afb590107907434de83d5f5f3e77a3f1
David Oster <paspa@hotmail.com>
2020-05-15 17:02:18 +0300

    QuickSort implemented for Size

--- src/tshirtsort/MainClass.java
+++ src/tshirtsort/MainClass.java
@@ -21,30 +21,67 @@ public class MainClass {
      */
     public static void main(String[] args) {
         TShirtFactory tFactory = new TShirtFactory();
-        List<TShirt> shirts = tFactory.tShirtGenerateX(10);
+        List<TShirt> shirts = tFactory.tShirtGenerateX(3);
         QuickSort qs = new QuickSort();
-        
+
         for (TShirt shirt : shirts) {
             System.out.println(shirt);
         }
-        quickSort(qs, shirts, true);
-        quickSort(qs, shirts, false);
+        quickSort(qs, shirts, true, 1); // Size ASC
+        quickSort(qs, shirts, false, 1); // Size DESC
         
+        quickSort(qs, shirts, true, 2); // Color ASC
+        quickSort(qs, shirts, false, 2); // Color DESC
         
+        quickSort(qs, shirts, true, 3); // Fabric ASC
+        quickSort(qs, shirts, false, 3); // Fabric DESC
+        
+
     }
-    
-    public static void quickSort(QuickSort qs, List<TShirt> shirts, boolean sortType) {
+
+    public static void quickSort(QuickSort qs, List<TShirt> shirts, boolean sortType, int sortByProperty) {
         System.out.println("/// --------- ///");
-        long startTime = System.currentTimeMillis();
-        qs.sort(shirts, 0, shirts.size() - 1, sortType);
-        long endTime = System.currentTimeMillis();
-        if(sortType) {
-            System.out.println("Time Lapsed for QS by Size ASC: " + (endTime - startTime));
-        } else {
-            System.out.println("Time Lapsed for QS by Size DESC: " + (endTime - startTime));
+        long startTime = 0;
+        long endTime = 0;
+        switch (sortByProperty) {
+            // Size - 1
+            case 1:
+                startTime = System.currentTimeMillis();
+                qs.sort(shirts, 0, shirts.size() - 1, sortType, 1);
+                endTime = System.currentTimeMillis();
+
+                if (sortType) {
+                    System.out.println("Time Lapsed for QS by Size ASC: " + (endTime - startTime));
+                } else {
+                    System.out.println("Time Lapsed for QS by Size DESC: " + (endTime - startTime));
+                }
+                break;
+            // Color - 2
+            case 2:
+                startTime = System.currentTimeMillis();
+                qs.sort(shirts, 0, shirts.size() - 1, sortType, 2);
+                endTime = System.currentTimeMillis();
+
+                if (sortType) {
+                    System.out.println("Time Lapsed for QS by Color ASC: " + (endTime - startTime));
+                } else {
+                    System.out.println("Time Lapsed for QS by Color DESC: " + (endTime - startTime));
+                }
+                break;
+            // Fabric - 3
+            case 3:
+                startTime = System.currentTimeMillis();
+                qs.sort(shirts, 0, shirts.size() - 1, sortType, 3);
+                endTime = System.currentTimeMillis();
+
+                if (sortType) {
+                    System.out.println("Time Lapsed for QS by Fabric ASC: " + (endTime - startTime));
+                } else {
+                    System.out.println("Time Lapsed for QS by Fabric DESC: " + (endTime - startTime));
+                }
+                break;
         }
-            
-        
+
         for (TShirt shirt : shirts) {
             System.out.println(shirt);
         }
--- src/tshirtsort/sorting/QuickSort.java
+++ src/tshirtsort/sorting/QuickSort.java
@@ -13,49 +13,106 @@ import tshirtsort.models.TShirt;
  * @author mac
  */
 public class QuickSort {
-    
+
     // low = arr.get(low), low = 4
     // high = arr.get(high), high = 0
     // boolean sortType, sortType == true, ASC --  sortType == false, DESC
-    public void sort(List<TShirt> arr, int low, int high, boolean sortType) {
+    // int sortByProperty 
+    // sortByProperty == 1 -- Size
+    // sortByProperty == 2 -- Color 
+    // sortByProperty == 3 -- Fabric 
+    public void sort(List<TShirt> arr, int low, int high, boolean sortType, int sortByProperty) {
         if (low < high) {
             /* pi is partitioning index, arr[pi] is  
               now at right place */
-            int pi = partition(arr, low, high, sortType);
+            int pi = partition(arr, low, high, sortType, sortByProperty);
 
             // Recursively sort elements before 
             // partition and after partition 
-            sort(arr, low, pi - 1, sortType);
-            sort(arr, pi + 1, high, sortType);
+            sort(arr, low, pi - 1, sortType, sortByProperty);
+            sort(arr, pi + 1, high, sortType, sortByProperty);
         }
     }
 
     // boolean sortType, sortType == true, ASC --  sortType == false, DESC
-    int partition(List<TShirt> arr, int low, int high, boolean sortType) {
+    // int sortByProperty 
+    // sortByProperty == 1 -- Size
+    // sortByProperty == 2 -- Color 
+    // sortByProperty == 3 -- Fabric 
+    int partition(List<TShirt> arr, int low, int high, boolean sortType, int sortByProperty) {
         TShirt pivot = arr.get(high);
         int i = (low - 1); // index of smaller element 
         for (int j = low; j < high; j++) {
             // If current element is smaller than the pivot 
-            if(sortType) {
-                if (arr.get(j).getSize().ordinal() < pivot.getSize().ordinal()) {
-                    i++;
+            switch (sortByProperty) {
+                // Size
+                case 1:
+                    if (sortType) {
+                        if (arr.get(j).getSize().ordinal() < pivot.getSize().ordinal()) {
+                            i++;
 
-                    // swap arr[i] and arr[j] 
-                    TShirt temp = arr.get(i);
-                    arr.set(i, arr.get(j));
-                    arr.set(j, temp);
-                }
-            } else {
-                if (arr.get(j).getSize().ordinal() > pivot.getSize().ordinal()) {
-                    i++;
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    } else {
+                        if (arr.get(j).getSize().ordinal() > pivot.getSize().ordinal()) {
+                            i++;
 
-                    // swap arr[i] and arr[j] 
-                    TShirt temp = arr.get(i);
-                    arr.set(i, arr.get(j));
-                    arr.set(j, temp);
-                }
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    }
+                    break;
+                // Color
+                case 2:
+                    if (sortType) {
+                        if (arr.get(j).getColor().ordinal() < pivot.getColor().ordinal()) {
+                            i++;
+
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    } else {
+                        if (arr.get(j).getColor().ordinal() > pivot.getColor().ordinal()) {
+                            i++;
+
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    }
+                    break;
+                // Fabric
+                case 3:
+                    if (sortType) {
+                        if (arr.get(j).getFabric().ordinal() < pivot.getFabric().ordinal()) {
+                            i++;
+
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    } else {
+                        if (arr.get(j).getFabric().ordinal() > pivot.getFabric().ordinal()) {
+                            i++;
+
+                            // swap arr[i] and arr[j] 
+                            TShirt temp = arr.get(i);
+                            arr.set(i, arr.get(j));
+                            arr.set(j, temp);
+                        }
+                    }
+                    break;
             }
-            
+
         }
 
         // swap arr[i+1] and arr[high] (or pivot) 
