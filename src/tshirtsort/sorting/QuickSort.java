a2d2fc4f69ec95e5988e705e8ec86a5671cf8f40
David Oster <paspa@hotmail.com>
2020-05-15 14:46:13 +0300

    Initial

---
+++ src/tshirtsort/sorting/QuickSort.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tshirtsort.sorting;
+
+/**
+ *
+ * @author mac
+ */
+public class QuickSort {
+
+    public void sort(int arr[], int low, int high) {
+        if (low < high) {
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element 
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than the pivot 
+            if (arr[j] < pivot) {
+                i++;
+
+                // swap arr[i] and arr[j] 
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}
